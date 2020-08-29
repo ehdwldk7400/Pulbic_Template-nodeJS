f78b7fba8c68a60182f870b404aab2c64495c650
ehdwldk7400 <68597164+ehdwldk7400@users.noreply.github.com>
2020-08-29 15:36:14 +0900

    Password 암호화

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const monggose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = monggose.Schema({
-    nam: {
+    name: {
         type: String,
         maxlength: 50
     },
@@ -9,6 +11,10 @@ const userSchema = monggose.Schema({
         type: String,
         trim: true,
         uniqu: 1
+    },
+    password: {
+        type: String,
+        minglength: 5
     },
     lastnmae: {
         type: String,
@@ -25,6 +31,26 @@ const userSchema = monggose.Schema({
     tokenExp: {
         type: Number
     }
+})
+userSchema.pre('save', function (next) {
+
+    let user = this;
+    // 비밀번호 암호화
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+
 })
 
 const User = monggose.model('User', userSchema)
