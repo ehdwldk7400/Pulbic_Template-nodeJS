d220b31a60ee453b937719611a713f5686c80a2d
ehdwldk7400 <68597164+ehdwldk7400@users.noreply.github.com>
2020-08-29 15:14:22 +0900

    DB 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:admin@exam.wf7gv.mongodb.net/youtube?retryWrites=true&w=majority', {
+
+// application/x-ww/form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(() => console.log('MongoDB Connected..')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요ㅕ한 정보들을 Cluent에서 가져오면
+    // 그것들을 데이터 베이스에 삽입한다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    });
 })
 
 app.listen(port, () => {
