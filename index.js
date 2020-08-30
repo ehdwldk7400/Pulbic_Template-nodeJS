8e9832b07af9c0435917a860b0a097521d28da62
ehdwldk7400 <68597164+ehdwldk7400@users.noreply.github.com>
2020-08-30 15:45:01 +0900

    auth / logout 기능 구현

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 const config = require('./config/key');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middelware/auth');
 
 const mongoose = require('mongoose');
 
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 mongoose.connect(config.mongoURI, {
@@ -23,7 +26,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원 가입 할때 필요ㅕ한 정보들을 Cluent에서 가져오면
     // 그것들을 데이터 베이스에 삽입한다.
@@ -34,6 +37,63 @@ app.post('/register', (req, res) => {
             success: true
         })
     });
+})
+app.post('/api/users/login', (req, res) => {
+
+    // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+
+
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+
+            // 비밀번호까지 맏다면 토큰 생성.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                //  토근을 저장한다.
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
+
+
+app.get('/api/users/auth', auth, (req, rs) => {
+
+    // 여기까지 미들웨어를 통과해 왔다는 것은 Authentication 이 True라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.userlastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" }, (err, user) => {
+            if (err) return res.jsonp({ success: false, err });
+            return res.status(200).send({
+                sucess: true
+            })
+        })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const monggose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = monggose.Schema({
     name: {
@@ -50,8 +51,45 @@ userSchema.pre('save', function (next) {
     } else {
         next()
     }
+});
 
-})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+userSchema.methods.generateToken = function (cb) {
+
+    var user = this;
+    // jsonwebToken 을 이용해서 토큰 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+
+    //    user._id + 'secretToken' = token
+}
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
 
 const User = monggose.model('User', userSchema)
 
